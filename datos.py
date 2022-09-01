af23f92832f49329fd056dca06d023551f2262e7
Jorge <jorge1b3@hotmail.es>
2022-08-31 22:34:42 -0500

    Probando con la barra de color

--- datos.py
+++ datos.py
@@ -1,8 +1,9 @@
 import math
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib as mpl
+#from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Use pandas to read the data from the csv file.
 df = pd.read_csv('pos.csv')
@@ -23,72 +24,51 @@ n = numOfNodes = 20
 
 G = nx.path_graph(0)
 
-
-
-for(i,j,a) in zip(x,y,letters):
-    for(k,l,b) in zip(x,y,letters):
-        if(i != k):
-            #print(a + " " + b + " " +str(math.sqrt((i-k)**2 + (j-l)**2)))
-            G.add_edge(a, b, weight=math.sqrt((i-k)**2 + (j-l)**2))
-
-
-
+for (i, j, a) in zip(x, y, letters):
+    for (k, l, b) in zip(x, y, letters):
+        if i != k:
+            G.add_edge(a, b, weight=math.sqrt((i - k) ** 2 + (j - l) ** 2))
 
 pos = nx.circular_layout(G)
-#create a diccionary with the x and y coordinates of the nodes
-# Create a dictionary with number as key and zipped with the x and y coordinates.
+# create a dictionary with the x and y coordinates of the nodes
+# Create a dictionary with number than key and zipped with the x and y coordinates.
 nodes = {letters[i]: (x[i], y[i]) for i in range(len(x))}
 
 # Draw the graph with the weights as the edge labels and rainbow colors.
 nx.draw(G, nodes, edge_color=[G[u][v]['weight'] for (u, v) in G.edges()],
         edge_cmap=plt.cm.rainbow, with_labels=True)
-#plt.savefig("prueba.png", transparent=True, dpi=150)
+# plt.savefig("prueba.png", transparent=True, dpi=150)
 plt.show()
 
 # Copy the graph and add the nodes to it.
 G2 = G.copy()
 
-
 # Delete the edges with weight greater than 250.
 G.remove_edges_from([(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] > 200])
-#G.add_edge('A', 'B', weight=100)
+# G.add_edge('A', 'B', weight=100)
+# Plot the colorbar for the weights.
 
 nx.draw(G, nodes, edge_color=[G[u][v]['weight'] for (u, v) in G.edges()],
         edge_cmap=plt.cm.rainbow, with_labels=True)
+divider = make_axes_locatable(plt.gca())
+ax_cb = divider.new_horizontal(size="5%", pad=0.05)
+pesosw = list([G[u][v]['weight'] for (u, v) in G.edges()])
+norm = mpl.colors.Normalize(vmin=min(pesosw), vmax=max(pesosw))
+cb1 = mpl.colorbar.ColorbarBase(ax=ax_cb,cmap=plt.cm.rainbow,norm=norm,orientation='vertical')
+plt.gcf().add_axes(ax_cb)
 plt.savefig("reducido.png", transparent=True, dpi=150)
 plt.show()
+
+print(ax_n)
+
 
 
 # Print the degree of each node.
 print(len(G.edges))
-
-
-print(len(pred))
 
 # Create a new graph of the minimum spanning tree of the original graph.
 MST = nx.minimum_spanning_tree(G)
 # Plot the graph.
 nx.draw(MST, nodes, with_labels=True)
 plt.show()
-
-
-
-# Create a new graph using arista as the edges of the new graph.
-#G2 = nx.Graph()
-#for(i,j,a) in zip(aristas,pesos):
-#    G2.add_edge(i, j, weight=a)
-#
-#nx.draw(G2, nodes, with_labels=True)
-#plt.show()
-# Delete the edges directed from the node to itself.
-# Plot the graph.
-
-
-
-
-
-# create a new graph with the diskstra algorithm as edges and the nodes of the original graph.
-#H = nx.Graph()
-#H.add_nodes_from(G.nodes())
-#H.add_weighted_edges_from([(u, v, d['weight']) for (u, v, d) in zip(pred.keys(), pred.values(), dist.values())])
 
